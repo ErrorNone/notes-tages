8aa471f8167499f55b3c60138c55465d2ee3ba01
igor <igor.homechko@gmail.com>
2022-06-16 16:29:02 +0700

    db

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get(
+        "https://github.com/ErrorNone/notes-tages/blob/master/src/store/dataBase.json"
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return <div>fg</div>;
 }
 
 export default App;
